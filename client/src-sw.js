6ffd4292bc7e756543e6eb05ab842052f0e9791a
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-03-20 20:52:49 -0400

    final commit hopefully

--- client/src-sw.js
+++ client/src-sw.js
@@ -34,6 +34,10 @@ registerRoute(
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0,200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       })
     ],
   })
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,9 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -56,6 +58,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
